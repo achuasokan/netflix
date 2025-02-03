50b00f7f4c4e012dabd22fa526c8a624523ab4fb
achuasokan <achuasokan555@gmail.com>
2025-02-03 07:17:20 +0530

    Intial added all the files and folder

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-5xl text-blue-500 border border-2 border-blue bg-green-500'>Tailwindcss</h1>
+    </>
+  )
+}
+
+export default App
